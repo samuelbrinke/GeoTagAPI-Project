6db74167d5c0da4ed66ff9e6be2d18e82a8c1b63
Jonatan Eriksson <58311888+jonatan-eriksson@users.noreply.github.com>
2021-04-28 16:02:23 +0200

    Add get and post endpoints for geotags

--- GeoTagAPI-Project/Controllers/GeoTagController.cs
+++ GeoTagAPI-Project/Controllers/GeoTagController.cs
@@ -5,13 +5,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GeoTagAPI_Project.Models;
+using GeoTagAPI_Project.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeoTagAPI_Project.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/geo-comments")]
     [ApiController]
     public class GeoTagController : ControllerBase
     {
-        
+        private readonly GeoTagDbContext _context;
+        public GeoTagController(GeoTagDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GeoMessage>> GetGeoMessage(int id)
+        {
+            var geoMessage = await _context.GeoMessages.FindAsync(id);
+
+            if (geoMessage == null)
+                return NotFound();
+
+            return Ok(geoMessage);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GeoMessage>>> GetGeoMessages()
+        {
+            return await _context.GeoMessages.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GeoMessage>> CreateGeoMessage(GeoMessage geoMessage)
+        {
+            var newGeoMessage = new GeoMessage
+            {
+                Message = geoMessage.Message,
+                Latitude = geoMessage.Latitude,
+                Longitude = geoMessage.Longitude
+            };
+            
+            await _context.AddAsync(newGeoMessage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGeoMessage), new { id = newGeoMessage.Id }, newGeoMessage);
+        }
     }
 }
