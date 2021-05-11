5fadc735c14983540bc72fb4b08d688ac7dd9223
samuelbrinke <samuel.brinkeback@hotmail.com>
2021-05-11 16:43:30 +0200

    Added route: delete geo message by id

--- GeoTagAPI-Project/Controllers/V1/GeoTagController.cs
+++ GeoTagAPI-Project/Controllers/V1/GeoTagController.cs
@@ -120,6 +120,30 @@ namespace GeoTagAPI_Project.Controllers.V1
 
             return CreatedAtAction(nameof(GetGeoMessage), new { id = newGeoMessage.Id }, getGeoMessage);
         }
+        /// <summary>
+        /// Deletes a Geo-Message
+        /// </summary>
+        /// <remarks>Authorization is required to delete a Geo-Message</remarks>
+        /// <param name="ApiKey"></param>
+        /// <param name="id"></param>
+        /// <response code="200">Geo-Message Deleted</response>
+        /// <response code="400">Failed to Delete Geo-Message</response>
+        /// <response code="401">Failed to authorize</response>
+        /// <returns>A newly deleted Geo-Message</returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<GeoMessage>> DeleteGeoMessage([FromQuery] Guid ApiKey, int id)
+        {
+            var geoMessage = await _context.GeoMessages.FirstOrDefaultAsync(g => g.Id == id);
+
+            _context.Remove(geoMessage);
+            await _context.SaveChangesAsync();
+
+            return Ok(geoMessage);
+        }
 
     }
 }
